4fda55e7b0a3494753ff9581030df300d254f0fb
Suvodip Ghosh <suvodipghosh17@gmail.com>
2025-03-30 22:03:19 +0530

    excercise

---
+++ todoapp/src/main/java/com/in28minutes/springboot/todoapp/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.todoapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
