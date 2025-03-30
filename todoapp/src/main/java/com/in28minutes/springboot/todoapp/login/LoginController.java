979e6e60dac2f27400fab973ed01b8f5e9dba462
Suvodip Ghosh <suvodipghosh17@gmail.com>
2025-03-30 23:16:05 +0530

    logging

--- todoapp/src/main/java/com/in28minutes/springboot/todoapp/login/LoginController.java
+++ todoapp/src/main/java/com/in28minutes/springboot/todoapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.todoapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     //    http://localhost:8080/login?name=John
     @RequestMapping("/login")
     public String login(@RequestParam String name, ModelMap modelMap) {
         modelMap.put("name", name);
+
+        logger.debug("Request Param is {}.", name);
+        logger.info("Request Param is {}.", name);
+        logger.warn("Request Param is {}.", name);
+
         System.out.println("name = " + name);
         return "login";
     }
