9bf3cefa4d9f76dc8af67bc3d1e4e5c6deb07757
Suvodip Ghosh <suvodipghosh17@gmail.com>
2025-04-07 22:49:31 +0530

    Validations for REST Api

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/social/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/social/user/User.java
@@ -1,10 +1,15 @@
 package com.in28minutes.rest.webservices.restful_web_services.social.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
     private Integer id;
+    @Size(min = 2, message = "Name should have atleast 2 characters")
     private String name;
+    @Past(message = "Birthdate should be in past")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
